0b4c8bf972482fff7bb670a0739969c30ef4c4e8
DefinitivVair0 <heinzmutze4@gmail.com>
2025-02-27 23:23:06 +0100

    Projektdateien hinzufügen.

---
+++ AudioProcessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using NAudio;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace AudioDashboard
+{
+    class AudioProcessor(MMDevice mmDevice)
+    {
+        private WasapiCapture capture = new WasapiCapture(mmDevice);
+
+        private (string Info, double Volume, double Deviation) outData = ("",0, 0);
+        public (string Info, double Volume, double Deviation) getData() { return outData; }
+
+        private int avg = 100;
+        public bool setAverage(int avg)
+        {
+            if (avg > 0)
+            {
+                this.avg = avg;
+                volumeStack.Clear();
+                volumeStack.Capacity = avg;
+                return true;
+            }
+            else return false;
+        }
+
+        public void Start()
+        {
+            capture.DataAvailable += (s, a) => Capture(s, a);
+            var wf = capture.WaveFormat;
+            outData.Info = $"Channels: {wf.Channels}\nEncoding: {wf.Encoding}\nBipS: {wf.BitsPerSample}\nSample Rate: {wf.SampleRate}\nExtra Size: {wf.ExtraSize}\nBlock Allign: {wf.BlockAlign}\nAverage BpS: {wf.AverageBytesPerSecond}";
+            capture.StartRecording();
+        }
+
+        public void Stop()
+        {
+            capture.StopRecording();
+            capture.Dispose();
+            mmDevice.Dispose();
+        }
+
+        private List<double> volumeStack = new List<double>(capacity:100) { };
+
+        public async Task Capture(object sender, WaveInEventArgs args)
+        {
+            float max = 0;
+            var buffer = new WaveBuffer(args.Buffer);
+
+            // interpret as 32 bit floating point audio
+            for (int index = 0; index < args.BytesRecorded / 4; index++)
+            {
+                var sample = buffer.FloatBuffer[index];
+
+                // absolute value 
+                if (sample < 0) sample = -sample;
+                // is this the max value?
+                if (sample > max) max = sample;
+            }
+
+            double vsSum = volumeStack.Sum();
+
+            outData.Volume = max * 100;
+            if (volumeStack.Count != 0) { outData.Deviation = outData.Volume - vsSum / (volumeStack.Count); } else outData.Deviation = 0;
+
+
+
+                volumeStack.Add(outData.Volume);
+            while (volumeStack.Count >= volumeStack.Capacity-1 ) volumeStack.RemoveAt(0);
+
+
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(()=> { MainWindow.mw.Update(outData); }));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using NAudio.CoreAudioApi;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using System.Drawing;
+
+namespace AudioDashboard;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public static MainWindow mw;
+    public MainWindow()
+    {
+        DataContext = this;
+        mw = this;
+        InitializeComponent();
+
+        infoLabel.Content = "Channels:\nEncoding:\nBpS:\nSample Rate:\nExtra Size:\nBlock Allign:\nAverage BpS:";
+
+        var enumerator = new MMDeviceEnumerator();
+        mmDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+        foreach (var wasapi in mmDevices)
+        {
+            deviceBox.Items.Add(wasapi.FriendlyName);
+        }
+    }
+
+
+    private AudioProcessor ap = null;
+    private MMDeviceCollection mmDevices;
+
+    private void ClosingEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        if (ap!=null) ap.Stop();
+    }
+
+    public void Update((string Info, double Volume, double Deviation) Data)
+    {
+        volumeBar.Value = Data.Volume;
+        var scb = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)(2.5 * Data.Volume), (byte)(255 - 2.5 * Data.Volume), 0));
+        volumeBar.Foreground = scb;
+        angularGauge.Value = Data.Deviation;
+        infoLabel.Content = Data.Info;
+    }
+
+    private void stopBtn_Click(object sender, RoutedEventArgs e)
+    {
+        ap.Stop();
+        ap = null;
+
+        volumeBar.Value = 0;
+        angularGauge.Value = 0;
+    }
+
+    private void startBtn_Click(object sender, RoutedEventArgs e)
+    {
+        if (deviceBox.SelectedItem != null && ap == null) { ap = new AudioProcessor((mmDevices.ElementAt(deviceBox.SelectedIndex))); ap.Start(); }
+    }
+
+    private void averageSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        if (ap!=null) ap.setAverage((int)averageSlider.Value);
+    }
+}
